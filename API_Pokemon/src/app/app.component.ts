dc75b4f5156b24b9fc390a8a83959ee18b057277
SongAlfio <Song.Alfio@iisgalvanimi.edu.it>
2023-10-24 09:46:20 +0000

    Non finito, errore

---
+++ API_Pokemon/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'API_Pokemon';
+  Pokemon_list: any[]=[];
+  Poke_Obs!:Observable<any>;
+  data!: any[];
+
+  constructor(public http: HttpClient){
+
+  };
+
+  ngOnInit(): void {
+    this.Poke_Obs = this.http.get("https://pokeapi.co/api/v2/type");
+    this.Poke_Obs.subscribe(this.Receive_Pokemon);
+  };
+  
+  Receive_Pokemon = (data: any) => {
+    console.log(data)
+    this.data = data;
+  };
+}
